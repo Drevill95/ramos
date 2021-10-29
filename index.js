56c9e7b88805e876e29ba6c319a7a22b26471677
Drevill95 <89485867+Drevill95@users.noreply.github.com>
2021-10-29 07:48:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1634,7 +1634,7 @@ break
 		if (!isUser) return reply(mess.only.daftarB)
                 reply(mess.only.musica)
                 play = body.slice(5)
-                anu = await fetchJson(`https://api.zeks.me/api/ytplaymp3?q=${play}&apikey=hamilton50`)
+                anu = await fetchJson(`https://api.zeks.me/api/ytplaymp3?q=${play}&apikey=10hamilton`)
                 if (anu.error) return reply(anu.error)
                 infomp3 = `*「Cancion Encontrada ✅」*\n📌 *Título:* ${anu.result.title}\n🔗 *Fuente:* ${anu.result.source}\n📟 *Tamaño:* ${anu.result.size}\n\n*ESPERE ENVIANDO SU ARCHIVO MP3 ⚠*\n\n by drevill`
                 buffer = await getBuffer(anu.result.thumbnail)
@@ -1648,7 +1648,7 @@ break
 		if (!isUser) return reply(mess.only.daftarB)
 	        reply(mess.only.musica2)
                 play = body.slice(5)
-                anu = await fetchJson(`https://api.zeks.me/api/ytplaymp3?q=${play}&apikey=shanduy50`)
+                anu = await fetchJson(`https://api.zeks.me/api/ytplaymp3?q=${play}&apikey=10hamilton`)
                 if (anu.error) return reply(anu.error)
                 infomp3 = `*「Cancion Encontrada ✅」*\n📌 *Título:* ${anu.result.title}\n🔗 *Fuente:* ${anu.result.source}\n📟 *Tamaño:* ${anu.result.size}\n\n*ESPERE ENVIANDO SU ARCHIVO MP3 ⚠*\n\n by drevill`
                 buffer = await getBuffer(anu.result.thumbnail)
