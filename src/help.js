05b8725809f5ed36d4d043717da19b2814818d7f
Drevill95 <89485867+Drevill95@users.noreply.github.com>
2021-11-03 10:04:38 -0600

    Update help.js

--- src/help.js
+++ src/help.js
@@ -2,102 +2,102 @@ const help = (prefix) => {
 	return `
 
 ▋▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄
-▋┋ ☠️ 𝕾 𝖎 𝖆 𝖗 𝖆 𝕭 𝖔 𝖙 ☠️
-▋┋
-▋┋ Hola por favor registrarte con:
-▋┋ !daftar  y tu nombre
-▋┋ !reg y tu nombre
-▋┋ Comando: ⌜ ${prefix} ⌟
-▋┋
-▋▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄
-▋┋ ╭─────────────╮
-■█   ✰*CREAR STICKERS*✰
-▋┋ ╰─────────────╯
-▋┋ *Imagen*
-▋┋ ${prefix}sticker
-▋┋ *texto*
-▋┋ ${prefix}attp
-▋┋ *Gif*
-▋┋ ${prefix}stickergif
-▋┋ *6 segundos de video*
-▋┋
-▋┋ ╭─────────────╮
-■█   ✰*CONVERTIDORES*✰
-▋┋ ╰─────────────╯
-▋┋ *De sticker a JPG*
-▋┋ ${prefix}toimg
-▋┋ *De video a MP3*
-▋┋ ${prefix}tomp3
-▋┋
-▋┋ ╭──────────────╮
-■█ ✰*AUDIOS Y DESCARGAS*✰
-▋┋ ╰──────────────╯
-▋┋ *Descargar musica de YT*
-▋┋ ${prefix}desmenu
-▋┋ *Audio*
-▋┋ ${prefix}tts es (mas texto)
-▋┋ ${prefix}idioma
-▋┋ *Monas chinas*
-▋┋ ${prefix}otak
-▋┋ *Interactua con el bot*
-▋┋ ${prefix}shantera
-▋┋
-▋┋ ╭──────────────╮
-■█       ✰*OTROS*✰
-▋┋ ╰──────────────╯
-▋┋ *Juega con el bot*
-▋┋ ${prefix}juegos
-▋┋ *Link de Whatsapp*
-▋┋ ${prefix}wame
-▋┋ *Coloca un texo*
-▋┋ ${prefix}qrcode
-▋┋ *Creador del bot*
-▋┋ ${prefix}creador
-▋┋ *Nswfmenu*
-▋┋ ${prefix}nsfwmenu
-▋┋
-▋┋ ╭──────────────╮
-■█  ✰*GRUPOS Y ADMINS*✰
-▋┋ ╰──────────────╯
-▋┋ *Cerrar el grupo solo admins*
-▋┋ ${prefix}close o cerrar
-▋┋ *Abrir grupo solo admins*	
-▋┋ ${prefix}open o abrir
-▋┋ *Eliminar un miembro*
-▋┋ ${prefix}kick o alv
-▋┋ *Dar admin a un miembro*
-▋┋ ${prefix}promote
-▋┋ *Quitar admin a un miembro*
-▋┋ ${prefix}demote
-▋┋ *Comando de bienvenida solo grupos*
-▋┋ ${prefix}welmenu
-▋┋ *Menu antilink*
-▋┋ ${prefix}antimenu
-▋┋ *Link del grupo*
-▋┋ ${prefix}linkgc
-▋┋ *Nombra admins del grupo*
-▋┋ ${prefix}adminlist
-▋┋ *Mencionar a todos*
-▋┋ ${prefix}hidetag o todos
-▋┋ *Agregar un miembro con el numero*
-▋┋ ${prefix}unir y el numero
-▋┋ *Cambiar foto del grupo*
-▋┋ ${prefix}fotog
-▋┋ *Cambiar descripcion del grupo*
-▋┋ ${prefix}dgc
-▋┋ *Cambiar nombre del grupo*
-▋┋  ${prefix}name
-▋┋ ╭──────────────╮
-■█  ✰*OWNER*✰
-▋┋ ╰──────────────╯
-▋┋ *Actualizar el bot*
-▋┋ ${prefix}update
-▋┋ *Apagar el bot*
-▋┋ ${prefix}off
-▋┋ *Ver los chats del bot*
-▋┋ ${prefix}grupos
-▋┋
-▋┋ ☠️D r e v i l l☠️
+★ ☠️ 𝕾 𝖎 𝖆 𝖗 𝖆 𝕭 𝖔 𝖙 ☠️
+〓 Hola por favor registrarte con:
+★
+〓 !daftar  y tu nombre
+★ !reg y tu nombre
+〓 Comando: ⌜ ${prefix} ⌟
+★
+〓▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄
+★ ╭─────────────╮
+〓   ✰*CREAR STICKERS*✰
+★ ╰─────────────╯
+〓 *Imagen*
+★ ${prefix}sticker
+〓 *texto*
+★${prefix}attp
+〓 *Gif*
+★ ${prefix}stickergif
+〓 *6 segundos de video*
+★
+〓 ╭─────────────╮
+★   ✰*CONVERTIDORES*✰
+〓 ╰─────────────╯
+★ *De sticker a JPG*
+〓 ${prefix}toimg
+★ *De video a MP3*
+〓 ${prefix}tomp3
+★
+〓 ╭──────────────╮
+★ ✰*AUDIOS Y DESCARGAS*✰
+〓 ╰──────────────╯
+★ *Descargar musica de YT*
+〓 ${prefix}desmenu
+★ *Audio*
+〓 ${prefix}tts es (mas texto)
+★ ${prefix}idioma
+〓 *Monas chinas*
+★ ${prefix}otak
+〓 *Interactua con el bot*
+★ ${prefix}shantera
+〓
+★ ╭──────────────╮
+〓       ✰*OTROS*✰
+★ ╰──────────────╯
+〓 *Juega con el bot*
+★ ${prefix}juegos
+〓 *Link de Whatsapp*
+★ ${prefix}wame
+〓 *Coloca un texo*
+★ ${prefix}qrcode
+〓 *Creador del bot*
+★ ${prefix}creador
+〓 *Nswfmenu*
+★ ${prefix}nsfwmenu
+〓
+★ ╭──────────────╮
+〓 ✰*GRUPOS Y ADMINS*✰
+★ ╰──────────────╯
+〓 *Cerrar el grupo solo admins*
+★ ${prefix}close o cerrar
+〓 *Abrir grupo solo admins*	
+★ ${prefix}open o abrir
+〓 *Eliminar un miembro*
+★ ${prefix}kick o alv
+〓 *Dar admin a un miembro*
+★ ${prefix}promote
+〓 *Quitar admin a un miembro*
+★ ${prefix}demote
+〓 *Comando de bienvenida solo grupos*
+★ ${prefix}welmenu
+〓 *Menu antilink*
+★ ${prefix}antimenu
+〓 *Link del grupo*
+★ ${prefix}linkgc
+〓 *Nombra admins del grupo*
+★ ${prefix}adminlist
+〓 *Mencionar a todos*
+★ ${prefix}hidetag o todos
+〓 *Agregar un miembro con el numero*
+★ ${prefix}unir y el numero
+〓 *Cambiar foto del grupo*
+★ ${prefix}fotog
+〓 *Cambiar descripcion del grupo*
+★ ${prefix}dgc
+〓 *Cambiar nombre del grupo*
+★  ${prefix}name
+〓 ╭──────────────╮
+★ ✰*OWNER*✰
+〓 ╰──────────────╯
+★ *Actualizar el bot*
+〓 ${prefix}update
+★ *Apagar el bot*
+〓 ${prefix}off
+★ *Ver los chats del bot*
+〓 ${prefix}grupos
+★
+〓 ☠️D r e v i l l☠️
 ▋▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄
 `
 }
